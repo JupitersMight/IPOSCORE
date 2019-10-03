93a30e41693a42bc30ed2b27199286fb4bf53a6f
JupitersMight <leonardo_duarte_rodrigues@hotmail.com>
2019-10-03 14:20:30 +0100

    working version of feature ranking. Updated reports

--- iposcore/public/javascript/barchart.js
+++ iposcore/public/javascript/barchart.js
@@ -1,5 +1,11 @@
 'use strict'
 
+function fade(opacity, d) {
+	d3.select('#bar_' + d.id)
+		.transition()
+		.style('opacity', opacity)
+}
+
 function render(data, properties, init){
 
 	properties.heightScale.domain([0,1])
@@ -16,7 +22,7 @@ function render(data, properties, init){
 	if (init)
 		properties.svg.append('text')
 		.attr('x', (properties.width / 2) + properties.margins.left)
-		.attr("y", (properties.margins.top / 2)-properties.margins.top)
+		.attr("y", (properties.margins.top / 2) - properties.margins.top)
 		.attr("text-anchor", "middle")
 		.style("font-size", "20px")
 		.style('fill', '#b3b3b3')
@@ -31,8 +37,16 @@ function render(data, properties, init){
 		.append('rect').merge(bar)
 		.attr('class', 'bar')
 		.attr('id', d => 'bar_' + d.column_name)
+		.on('mouseover', d => {
+			fade(0.5, d)
+			//tip.show(d)
+		})
+		.on('mouseout', d => {
+			fade(1, d)
+			//tip.hide(d)
+		})
 		.transition('bar').duration(500)
-		.attr('fill', d => d.selected ? '#ec7014' : '#238443')
+		.attr('fill', '#158896')
 		.attr('x', d => properties.margins.left + properties.widthScale(d.column_name))
 		.attr('y', d => properties.margins.top + properties.heightScale(d.column_value))
 		.attr('width', properties.widthScale.bandwidth())
